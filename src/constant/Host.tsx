e41bfb17978782bfbb17703146c728330068fe97
xuanhoang <102515365+NXHoang1212@users.noreply.github.com>
2024-03-22 10:20:18 +0700

    push code

--- src/constant/Host.tsx
+++ src/constant/Host.tsx
@@ -1,11 +1,10 @@
 export const enum HOST {
-  // API = 'http://192.168.1.20:3000',
   API = 'http://192.168.1.21:3000',
   // API = 'https://nestsever-4273896b755f.herokuapp.com',
   SOCKET = 'http://192.168.1.21:3000/product',
   //API: 'http://192.168.55.174:3000'
   // APIRELEASE = 'https://nestsever-4273896b755f.herokuapp.com',
-};
+};  
 
 export const enum KEY {
   GOOGLEMAP = 'AIzaSyDFPSKwgFMBgSA0NjWimRQhF0l-IDs_fe4',
